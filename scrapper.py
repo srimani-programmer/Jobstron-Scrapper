70d5fe060b4c001ee1be7c14bb47e4a052de9fb0
srimani-programmer <srimani.scholar@gmail.com>
2019-08-24 16:10:10 +0530

    Improved the Quality of Storing Data.

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
-import pandas as pd
+# import pandas as pd
+import xlwt
 
 driver = webdriver.Chrome('/Users/srimanikanta/Downloads/chromedriver')
 driver.get('https://www.jobstron.com/aptitude-test-2')
@@ -11,6 +12,9 @@ res = driver.execute_script('return document.documentElement.outerHTML')
 
 soup = BeautifulSoup(res,'lxml')
 
+file_name = 'QandAMain.xls'
+excel_file = xlwt.Workbook()
+sheet = excel_file.add_sheet('Sheet1')
 scrapper = list()
 # Question list Extracter.
 count = 1
@@ -20,6 +24,24 @@ questions_list = soup.find_all('div', class_="wpProQuiz_question_text")
 options_list = soup.find_all('ul',class_="wpProQuiz_questionList")
 # print(type(questions_list))
 
+Qrow = 1
+Qcol = 2
+Orow = Qrow + 1
+Ocol = 2
+sheet.write(0,0, 'QNO')
+sheet.write(0,1, 'Question')
+
+for i,j in zip(questions_list,options_list):
+    sheet.write(Qrow,Qcol,str(i.text.replace('\n', '').strip()))
+    Orow = Qrow + 1
+    sheet.write(Orow,Ocol,str(j.text.replace('\n','').strip().replace(" ", "")))
+    Qrow = Orow + 1
+
+
+excel_file.save(file_name) 
+
+
+'''
 for i,j in zip(questions_list, options_list):
 
     scrapper.append(str(i.text.replace('\n', '').strip()))
@@ -36,7 +58,7 @@ writer = pd.ExcelWriter('QandA.xlsx', engine='xlsxwriter')
 df.to_excel(writer, sheet_name='Sheet1')
 
 writer.save()
-
+'''
 driver.quit()
 
 #print(options_list[0].text.replace('\n','').strip().replace(' ', ""))
